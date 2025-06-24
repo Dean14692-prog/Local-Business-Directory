640483e358ef3a6d2b21026d6537494db5d96161
nosmall <elvisotieno24@gmail.com>
2025-06-24 08:46:52 +0300

    Define initial database models

--- server/models.py
+++ server/models.py
@@ -1,53 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy
+from datetime import datetime
 
-
+db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__="users"
-    id = db.Column(db.Integer,primary_key=True)
-    name =db.Column(db.String,nullable=False)
-    password_hash =db.Column(db.String,nullable=False)
-    role=db.Column(db.String,default="customer")
-    #Relationship
-    business_profiles = db.relationship('BusinessProfile', back_populates='owner')
-    reviews = db.relationship('Review', back_populates='author')
+    __tablename__ = 'users'
 
-    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String)
+    role = db.Column(db.Enum('consumer', 'business_owner', name='user_roles'), default='consumer')
+#relationship
+    reviews = db.relationship('Review', back_populates='author')
+    business_profile = db.relationship('BusinessProfile', uselist=False, back_populates='owner')
+
 
 class BusinessProfile(db.Model):
-    __tablename__="business_profiles"
-    id = db.Column(db.Integer,primary_key=True)
-    name =db.Column(db.String,nullable=False)
-    description =db.Column(db.String,nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    __tablename__ = 'business_profiles'
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    contact = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    description = db.Column(db.Text)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), unique=True)
+    owner = db.relationship('User', back_populates='business_profile')
 #relationship
-
-    owner = db.relationship('User', back_populates='business_profiles')
+    category = db.relationship('Category', back_populates='business_profile', uselist=False)
     reviews = db.relationship('Review', back_populates='business_profile')
-    category = db.relationship('Category', back_populates='business_profiles')
 
 
 class Category(db.Model):
-    __tablename__="categorys"
-    id = db.Column(db.Integer,primary_key=True)
-    name =db.Column(db.String,nullable=False)
-    description =db.Column(db.String,nullable=False)
-#Relationship
+    __tablename__ = 'categories'
 
-business_profiles = db.relationship('BusinessProfile', back_populates='category')
-
-class Review(db.models):
-    __tablename__="reviews"
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    business_profile_id = db.Column(db.Integer, db.ForeignKey('business_profile.id'))
+    name = db.Column(db.String)
+    business_profile_id = db.Column(db.Integer, db.ForeignKey('business_profiles.id'), unique=True)
+#relationship
+    business_profile = db.relationship('BusinessProfile', back_populates='category')
+
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    business_profile_id = db.Column(db.Integer, db.ForeignKey('business_profiles.id'))
     rating = db.Column(db.Integer)
     comment = db.Column(db.Text)
-#Relationship
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+#relationship
     user = db.relationship('User', back_populates='reviews')
     business_profile = db.relationship('BusinessProfile', back_populates='reviews')
-
